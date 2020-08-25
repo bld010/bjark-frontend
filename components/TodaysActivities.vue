31da06934315f788a4b4f74930af1c2ae8edf4b5
Brianna DelValle <brianna.delvalle@flyfrontier.com>
2020-08-25 16:18:15 -0600

    Play with list rendering

--- components/TodaysActivities.vue
+++ components/TodaysActivities.vue
@@ -1,6 +1,14 @@
 <template>
-  <div  class="w-full h-full">
-    <p>The dog went {{activities[0]}} + {{activities[1]}} </p>
+  <div  class="w-1/2 bg-purple-500 flex justify-center items-center flex-col m-auto h-full">
+    <h2 class="text-xl text-center mb-3 underline">Today's Dog Log</h2>
+    <ul class="">
+      <li class="flex flex-row justify-between" v-for="activity in activities" :key="activity.index">
+        <img class="h-8" v-if="activity.activity == 'poop'" src="../assets/poopMoji.png">
+        <img class="h-8" v-else src="../assets/pee.jpg">
+        <p>{{ activity.time }}</p>
+        <p>{{ activity.activity }} </p>
+      </li>
+    </ul>
 
   </div>
 </template>
@@ -10,11 +18,43 @@
 </style>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
+
+let activities = [
+  {
+    time: Date.now(),
+    activity: 'poop'
+  }, 
+  { time: Date.now() + 1, 
+    activity: 'pee'
+  },
+  {
+    time: Date.now(),
+    activity: 'poop'
+  }, 
+  { time: Date.now() + 1, 
+    activity: 'pee'
+  },
+  {
+    time: Date.now(),
+    activity: 'poop'
+  }, 
+  { time: Date.now() + 1, 
+    activity: 'pee'
+  },
+  {
+    time: Date.now(),
+    activity: 'poop'
+  }, 
+  { time: Date.now() + 1, 
+    activity: 'pee'
+  }
+]
+
 export default Vue.extend({
   data() {
     return {
-      activities: ['poop', 'pee']
+      activities: activities
     }
   }
 })
